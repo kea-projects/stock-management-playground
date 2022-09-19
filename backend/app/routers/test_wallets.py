feb10a56b2cf41e23ff14fb816d44ddfe9e7babe
CristiPV <cristi.valentin29@gmail.com>
2022-09-19 11:02:14 +0200

    remove old test

--- backend/app/routers/test_wallets.py
+++ backend/app/routers/test_wallets.py
@@ -5,7 +5,10 @@ from ..main import app
 client = TestClient(app)
 
 
-def test_read_wallets():
-    response = client.get("/wallets/")
-    assert response.status_code == 200
-    assert response.json() == {"response": "WALLETS !!!"}
+# def test_read_wallets():
+#     response = client.get("/wallets/")
+#     assert response.status_code == 200
+#     assert response.json() == {"response": "WALLETS !!!"}
+
+# We need to look into how to make tests work with
+# beanie and how to mock a database.
