fefc6715c9d145baffc92ac5920522df653d4aca
CristiPV <cristi.valentin29@gmail.com>
2022-09-19 10:44:11 +0200

    add environment variables

--- backend/app/utils/mongo.py
+++ backend/app/utils/mongo.py
@@ -1,12 +1,17 @@
 from beanie import init_beanie
 import motor
+from dotenv import dotenv_values
 
 from ..models.wallet import Wallet
+
+config = dotenv_values(".env")
 
 
 async def init_db():
     client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb://cristi:2912@localhost:27017")
+        f"mongodb://{config['MONGO_USER']}" +
+        f":{config['MONGO_PASSWORD']}@{config['MONGO_HOST']}"
+    )
     await init_beanie(database=client.stock_management_playground,
                       document_models=[Wallet])
     print("MongoDB initialized!")
