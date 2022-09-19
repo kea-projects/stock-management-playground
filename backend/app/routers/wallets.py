3df58e46152243600ac712fea41fdd8a463c5038
CristiPV <cristi.valentin29@gmail.com>
2022-09-19 10:30:23 +0200

    add crud wallet endpoints

--- backend/app/routers/wallets.py
+++ backend/app/routers/wallets.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from beanie import PydanticObjectId
+
+from ..models.wallet import Wallet
 
 router = APIRouter(
     prefix="/wallets"
@@ -7,4 +10,35 @@ router = APIRouter(
 
 @router.get("/")
 async def read_wallets():
-    return {"response": "WALLETS !!!"}
+    return await Wallet.find_all().to_list()
+
+
+@router.get("/{wallet_id}")
+async def read_wallet_by_id(wallet_id: PydanticObjectId):
+    return await Wallet.get(wallet_id)
+
+
+@router.post("/")
+async def create_wallet(wallet: Wallet):
+    return await wallet.create()
+
+
+@router.patch("/{wallet_id}")
+async def update_wallet(wallet: Wallet, wallet_id: PydanticObjectId):
+    wallet_to_update = await Wallet.get(wallet_id)
+    if (wallet_to_update is not None):
+        wallet_to_update.nickname = wallet.nickname
+        wallet_to_update.balance = wallet.balance
+        return await wallet_to_update.replace()
+    else:
+        raise HTTPException(status_code=404, detail="Wallet not found!")
+
+
+@router.delete("/{wallet_id}")
+async def delete_wallet(wallet_id: PydanticObjectId):
+    wallet_to_delete = await Wallet.get(wallet_id)
+    if (wallet_to_delete is not None):
+        wallet_to_delete.delete()
+        return {"detail": "Deleted."}
+    else:
+        raise HTTPException(status_code=404, detail="Wallet not found!")
