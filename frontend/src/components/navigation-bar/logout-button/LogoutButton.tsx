bdb3c701eef845e3e0fc317da8785ef2a6c4d367
Itai Gramse <itai0004@stud.kea.dk>
2022-12-20 10:02:49 +0100

    clean react query cache on logout

--- frontend/src/components/navigation-bar/logout-button/LogoutButton.tsx
+++ frontend/src/components/navigation-bar/logout-button/LogoutButton.tsx
@@ -3,12 +3,20 @@ import { useContext } from 'react'
 import { UserContext } from '../../../provider/user-provider/UserProvider'
 import { Link } from 'react-router-dom'
 import { routes } from '../../../routes'
+import { useQueryClient } from '@tanstack/react-query'
+import { UserKeys } from "../../../api/hooks/useUser";
+import { WalletsKeys } from "../../../api/hooks/useWallets";
 
 export function LogoutButton() {
+    const queryClient = useQueryClient
     const { setUserDetail } = useContext(UserContext)
+    const logout =async () => {
+      await queryClient().invalidateQueries([UserKeys.userMeKey, WalletsKeys.singleWalletMe, WalletsKeys.allWalletsMe])
+      setUserDetail({})
+    }
     return (
         <Box>
-            <Link to={routes.homepage} onClick={() => setUserDetail({})}>
+            <Link to={routes.homepage} onClick={() => logout()}>
                 Log Out
             </Link>
         </Box>
