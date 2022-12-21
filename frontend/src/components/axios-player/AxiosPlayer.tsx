904da1c3aab8bb564185f24651271af0d89b722c
CristiPV <cristi.valentin29@gmail.com>
2022-12-21 08:12:30 +0100

    remove sentry interceptor logs

--- frontend/src/components/axios-player/AxiosPlayer.tsx
+++ frontend/src/components/axios-player/AxiosPlayer.tsx
@@ -1,7 +1,6 @@
-import { ReactNode, useContext } from "react";
+import { ReactNode, useContext } from 'react'
 import { UserContext } from '../../provider/user-provider/UserProvider'
 import { axiosInstance } from '../../api/client-config/axiosConfig'
-import * as Sentry from '@sentry/react'
 
 interface AxiosAdminProps {
     children: ReactNode
@@ -17,17 +16,6 @@ export const AxiosPlayer = ({ children }: AxiosAdminProps) => {
                 ] = `${userDetail.tokenType} ${userDetail.token}`
             }
             return config
-        },
-        async (error) => {
-            return Promise.reject(error)
-        }
-    )
-    axiosInstance.interceptors.response.use(
-        async (response) => {
-            Sentry.setExtra('response_status', response.status)
-            Sentry.setExtra('response_data', JSON.stringify(response.data))
-            Sentry.captureMessage('API response')
-            return response
         },
         async (error) => {
             return Promise.reject(error)
